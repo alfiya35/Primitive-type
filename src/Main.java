20bae06b037f9c75824330cf84bc6a66b86603fc
Альфия Устименко <alfiya.ustimenko@ukr.net>
2022-04-01 17:30:15 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticket = 250;
+        int miles = 20;
+
+        int bonus = ticket / miles;
+        System.out.println("Ваш бонус составит: " + bonus);
+    }
+
+}
